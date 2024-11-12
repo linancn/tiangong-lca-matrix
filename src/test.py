c72248dc7444d963ccb58fec8cc114087190d387
Nan <linanenv@gmail.com>
2024-11-12 16:37:35 +0800

    Change matrix data type from float64 to float32 in test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import cupy as cp
 
-# float64 matrix
-matrix = cp.random.rand(20000, 20000).astype("float64")
+# float32 matrix
+matrix = cp.random.rand(20000, 20000).astype("f")
 
 # inverse
 inverse_matrix = cp.linalg.inv(matrix)
